a495af74d70c03e9683238be272926696b0d49b8
Nikolas Nyby <nikolas@gnu.org>
2018-12-10 14:07:08 -0500

    :bookmark: release version 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="surelink",
-    version="1.0.1",
+    version="1.1.1",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="https://github.com/ccnmtl/surelink/",
